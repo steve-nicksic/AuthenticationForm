83457b931859399c2648710cb6c69a4891763973
steve-nicksic <65583459+steve-nicksic@users.noreply.github.com>
2021-02-09 11:01:54 -0600

    Initial commit

---
+++ src/main/java/com/tts/authenticationform/AuthenticationFormApplication.java
@@ -0,0 +1,13 @@
+package com.tts.authenticationform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/authenticationform/AuthenticationFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.authenticationform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
